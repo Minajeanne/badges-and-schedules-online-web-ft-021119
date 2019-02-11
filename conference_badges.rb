dc4f85af10efb22e8608a24fddbee38e635f4875
Alicia Santiago <albrown07@mac.com>
2019-02-11 22:47:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,7 +24,7 @@ end
 
 def printer(array)
  badges = batch_badge_creator(array)
- badges.each {|badge| puts "#{badge}."}
+ badges.each {|badge| puts "#{badge}"}
   
   assign_rooms(array).each {|room| puts "Hello, #{name}! You'll be assigned to room #{room}!"}
   
